c4f11cd08f9c988f22c355f20ad9b96fac923fae
CodeWithAnishShrestha <growwithanish@gmail.com>
2024-04-17 14:41:04 +0545

    Creating category controller and view for creating new category

---
+++ BulkyWeb/Migrations/20240416104243_AddCategoryTabletoDb1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BulkyWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCategoryTabletoDb1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Category21Id",
+                table: "Categories",
+                newName: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Categories",
+                newName: "Category21Id");
+        }
+    }
+}
